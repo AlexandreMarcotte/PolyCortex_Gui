1d18e76cd30d0ee5ded2d0896a8e30d818233f23
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-11-25 21:48:06 -0500

    Commented shift in uniformize data so that we see the comple signal for EEG experiments

--- data_processing_pipeline/uniformize_data.py
+++ data_processing_pipeline/uniformize_data.py
@@ -5,7 +5,7 @@ def uniformize_data(s, len_signal):
     """Uniformize signal (s) """
     s = s - np.average(s)
     avg = np.average(s)
-    s = shift(s, len_signal / 2 - np.argmin(s), cval=avg)
+    # s = shift(s, len_signal / 2 - np.argmin(s), cval=avg)
     min_sig = np.min(s)
     max_sig = np.max(s)
     s = s / (max_sig - min_sig)
