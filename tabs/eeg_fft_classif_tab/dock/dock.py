0758064d5e533f4a0d8cd487133125ff4f07884a
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-11-30 13:19:16 -0500

    Created dock as a base class for the Docks

--- tabs/eeg_fft_classif_tab/dock/dock.py
+++ tabs/eeg_fft_classif_tab/dock/dock.py
@@ -1,17 +1,30 @@
+# -- General packages --
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtCore
 import pyqtgraph as pg
+# -- My packages --
 from app.colors import *
+from app.activation_b import btn
 
 
 class Dock:
-    def __init__(self):
-        self.timer = QtCore.QTimer()
+    def __init__(self, layout):
+        self.layout = layout
+
+        plot_gr, self.plot_layout = self.create_gr()
         self.init_plot()
 
+        self.layout.addWidget(plot_gr, 0, 0)
+
+        self.init_on_off_button()
+
+        self.timer = QtCore.QTimer()
+
     def init_plot(self):
-        self.plot = pg.PlotWidget(background=dark_grey)
+        plot = pg.PlotWidget(background=dark_grey)
+        self.plot_layout.addWidget(plot, 1, 0)
+        return plot
 
     def create_gr(self):
         gr = QGroupBox()
@@ -19,9 +32,13 @@ class Dock:
         gr.setLayout(l)
         return gr, l
 
+    def init_on_off_button(self):
+        btn('Show wave signal', self.layout, (0, 0), func_conn=self.start,
+            color=blue_b, toggle=True, txt_color=white)
+
     @pyqtSlot(bool)
     def start(self, checked):
         if checked:
-            self.timer.start(100)
+            self.timer.start(2000)
         else:
             self.timer.stop()
