905baf28dabeaf285ec61a43b533078663b65c86
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-12-03 00:41:05 -0500

    connect band graph to the fft signal

--- tabs/eeg_fft_classif_tab/dock/wave_dock/wave_graph.py
+++ tabs/eeg_fft_classif_tab/dock/wave_dock/wave_graph.py
@@ -8,6 +8,8 @@ import pyqtgraph as pg
 from app.colors import *
 from ... dock.dock import Dock
 
+from timeit import timeit, Timer
+
 
 class WaveGraph(Dock):
     def __init__(self, gv, layout):
@@ -15,26 +17,43 @@ class WaveGraph(Dock):
         self.gv = gv
         self.layout = layout
 
+        self.waves = {'delta': Wave((0, 4)),
+                      'theta': Wave((4, 8)),
+                      'alpha': Wave((8, 12)),
+                      'beta': Wave((12, 40)),
+                      'gamma': Wave((40, 100))
+        }
+        self.slices = [slice(w.freq_range[0], w.freq_range[1])
+                       for w in self.waves.values()]
         self.plot = self.init_plot()
+        self.brushes, self.x, self.width = self.init_bar_braph_caract()
+        # self.time_func()
+        self.N_ELE = len(self.waves)
 
         self.timer.timeout.connect(self.update)
 
     def init_plot(self):
-        """
-        """
-        plot = super().init_plot()
+        """"""
+        plot = pg.PlotWidget(background=dark_grey)
+        self.plot_layout.addWidget(plot, 1, 0)
+        waves_names = [wave_name for wave_name in self.waves]
         plot.plotItem.setLabel(axis='left', text='Power', units='None')
-        plot.plotItem.hideAxis('bottom')
-        # Add graph
-        x = np.arange(10)
-        y = np.sin(x)
-        bg = pg.BarGraphItem(x=x, height=y, width=1, brush='b')
-        plot.addItem(bg)
-
+        plot.plotItem.setLabel(axis='bottom', text=waves_names)
         # Add to tab layout
         self.layout.addWidget(plot, 1, 0)
 
         return plot
+
+    def init_bar_braph_caract(self):
+        brushes = [pg.mkBrush(i) for i in range(len(self.waves))]
+        x = [w.get_half_pos() for w in self.waves.values()]
+        width = [w.get_delta_range() for w in self.waves.values()]
+
+        return brushes, x, width
+
+    def time_func(self):
+        # Timer(self.init_bar_braph_caract())
+        pass
 
     def add_head_img(self):
         mne_head = QLabel()
@@ -44,8 +63,18 @@ class WaveGraph(Dock):
     def update(self):
         # Remove All item from the graph
         self.plot.clear()
-        N_ELE = 5
-        y = np.random.random(N_ELE)
-        x = np.array([100, ])
-        bg = pg.BarGraphItem(x=x, height=y, width=[4, 4, 4, 28, 60], brush='b')
+        # y = np.random.random(self.N_ELE)
+        y = [np.average(self.gv.fft[0][s]) for s in self.slices]
+        bg = pg.BarGraphItem(x=self.x, height=y, width=self.width, brushes=self.brushes)
         self.plot.addItem(bg)
+
+
+class Wave:
+    def __init__(self, freq_range):
+        self.freq_range = freq_range
+
+    def get_half_pos(self):
+        return self.freq_range[0] + (self.get_delta_range()) / 2
+
+    def get_delta_range(self):
+        return self.freq_range[1] - self.freq_range[0]
