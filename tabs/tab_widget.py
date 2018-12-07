4c54247e7606c470df289cd48ef5a96025f32714
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-12-06 19:32:37 -0500

    Removed Brain 3D tab
    As the code is now present in the main tab (avoid redundancy)

--- tabs/tab_widget.py
+++ tabs/tab_widget.py
@@ -1,10 +1,11 @@
+# --General Packages--
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabWidget
-
+# --My Packages--
 from tabs.eeg_fft_classif_tab.eeg_fft_classif_tab import EegFftClassifTab
 from tabs.experiment_tab.experiment_tab import ExperimentTab
 from tabs.static_graph_tab.static_graph_tab import StaticGraphTab
 from tabs.mini_game_tab.mini_game_tab import MiniGameTab
-from tabs.brain_3D_tab.brain_3D_tab import Brain3DTab
+# from tabs.brain_3D_tab.brain_3D_tab import Brain3DTab
 
 
 class TabWidget(QWidget):
@@ -14,14 +15,11 @@ class TabWidget(QWidget):
         self.tabs_list = {EegFftClassifTab(gv, parent): 'Live graph',
                           ExperimentTab(gv): 'Experiments',
                           StaticGraphTab(gv): 'Static graph',
-                          MiniGameTab(): 'Mini Game',
-                          Brain3DTab(): '3D brain'}
+                          MiniGameTab(): 'Mini Game'}
 
         self.layout = QVBoxLayout(self)
-
         # Initialize tab screen
         self.tabs = QTabWidget()
-
         # Add tabs
         for tab, name in self.tabs_list.items():
             self.tabs.addTab(tab, name)
