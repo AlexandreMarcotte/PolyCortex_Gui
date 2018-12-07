7f2f6912a80af3bd58def06d8e8ff20c5ff45c07
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-12-06 19:37:32 -0500

    Change open bci streaming so that it fit only in one class like the other streaming sources

--- generate_signal/from_openbci.py
+++ generate_signal/from_openbci.py
@@ -5,30 +5,23 @@ from time import sleep
 import openbci_interface.open_bci_v3 as bci
 
 
-def stream_data_from_OpenBCI(gv):
-    port = '/dev/ttyUSB0'  # if using Linux
-    # (if encounter error: [Errno 13] could not open port /dev/ttyUSB0: Permission denied => see: https://askubuntu.com/questions/58119/changing-permissions-on-serial-port   then restart your computer
-    # port = 'COM3'  # if using Windows
-    # port = '/dev/tty.OpenBCI-DN008VTF'  # If using MAC?
-    logging.basicConfig(filename="test.log", format='%(asctime)s - %(levelname)s : %(message)s', level=logging.DEBUG)
-    logging.info('---------LOG START-------------')
-    board = bci.OpenBCIBoard(port=port, scaled_output=False, log=True)
-    board.enable_filters()
-    print("Board Instantiated")
-    sleep(5)
-
-    OpenBCI_sampler = SampleDataFromOPENBCI(board, gv)
-    OpenBCI_sampler.start()
-
-    return board
-
-
 class SampleDataFromOPENBCI(threading.Thread):
-    def __init__(self, board, gv):
+    def __init__(self, gv):
         super().__init__()
-        self.board = board
         self.gv = gv
+        port = '/dev/ttyUSB0'  # if using Linux
+        # (if encounter error: [Errno 13] could not open port /dev/ttyUSB0:
+        #  Permission denied
+        #  => see: https://askubuntu.com/questions/58119/changing-permissions-on-serial-port
+        #    then restart your computer
+        # port = 'COM3'  # if using Windows
+        # port = '/dev/tty.OpenBCI-DN008VTF'  # If using MAC?
+        # logging.basicConfig(filename="test.log", format='%(asctime)s - %(levelname)s : %(message)s', level=logging.DEBUG)
+        # logging.info('---------LOG START-------------')
+        self.board = bci.OpenBCIBoard(port=port, scaled_output=False, log=True)
+        self.board.enable_filters()
+        print("Board Instantiated")
+        sleep(1)  # TODO: I changed it to 1 and it was 5 before see if there is a problem
 
     def run(self):
-        # Previously and Working
         self.board.start_streaming(self.gv.collect_data)
