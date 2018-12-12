f16d335e7fd7c5dd6ce2f0a44ac51e23a9a377f2
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-12-12 18:44:30 -0500

    change calcul fft so that it calcul the fft for wave plot for every ch

--- data_processing_pipeline/calcul_fft.py
+++ data_processing_pipeline/calcul_fft.py
@@ -8,6 +8,7 @@ class FreqCalculator:
         self.gv = gv
         self.remove_first_data = remove_first_data
 
+        self.freq_per_band_all_ch = [None for _ in range(self.gv.N_CH)]
         self.activated = False
         self.freq_range = 100
         self.timer = QtCore.QTimer()
@@ -37,20 +38,20 @@ class FreqCalculator:
             self.freq_range = self.get_freq_range()
             self.fft[ch] = self.calcul_fft(ch)
             self.fft_over_time[ch].append(self.fft[ch])
-            self.get_avg_freq_per_band()
+            self.freq_per_band_all_ch[ch] = self.get_avg_freq_per_band(ch)
 
-    def get_avg_freq_per_band(self):
-        self.freq_per_band = [np.average(self.fft[0][s]) for s in self.slices]
-        for ch, f in enumerate(self.freq_per_band):
+    def get_avg_freq_per_band(self, ch):
+        freq_per_band = [np.average(self.fft[ch][s]) for s in self.slices]
+        for ch, f in enumerate(freq_per_band):
             self.freq_band_over_time[ch].append(f)
-        return self.freq_per_band
+        return freq_per_band
 
     def get_freq_range(self):
         """Calculate FFT (Remove freq 0 because it gives a really high
          value on the graph"""
-        return np.linspace(self.remove_first_data,
-                           self.N_DATA//2/self.get_delta_t(),
-                           self.N_DATA//2 - self.remove_first_data)
+        return np.linspace(
+                self.remove_first_data, self.N_DATA//2/self.get_delta_t(),
+                self.N_DATA//2 - self.remove_first_data)
 
     def get_delta_t(self):
         """interval of time from the first to the last value that was
