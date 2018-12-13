07734560066c3df7877f2dfcb5316253af8e65f1
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-12-13 11:14:07 -0500

    Moved  self.waves into dispatcher

--- app/dispatcher.py
+++ app/dispatcher.py
@@ -54,6 +54,13 @@ class Dispatcher:
         self.emg_signal_len = 170
         # FFT
         self.freq_calculator = FreqCalculator(self, remove_first_data=0)
+        # Wave graph
+        self.waves = {'delta': Wave((0, 4)),
+                      'theta': Wave((4, 8)),
+                      'alpha': Wave((8, 12)),
+                      'beta': Wave((12, 40)),
+                      'gamma': Wave((40, 100))
+                      }
         # Viz3D
         self.plane_to_move = 'x'
 
@@ -121,4 +128,12 @@ class FilterProcess:
                                  maxlen=DEQUE_LEN) for _ in range(N_CH)]
 
 
+class Wave:
+    def __init__(self, freq_range):
+        self.freq_range = freq_range
 
+    def get_half_pos(self):
+        return self.freq_range[0] + (self.get_delta_range()) / 2
+
+    def get_delta_range(self):
+        return self.freq_range[1] - self.freq_range[0]
