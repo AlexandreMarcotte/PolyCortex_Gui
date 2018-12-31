a636106423aa8b49f1ec03057342f7d39eccbcc9
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-12-31 15:21:40 -0500

    Improve rotation button (east and font size)

--- app/rotated_button.py
+++ app/rotated_button.py
@@ -1,16 +1,23 @@
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 
+
 class RotatedButton(QPushButton):
-    def __init__(self, text, parent, orientation="west"):
+    def __init__(self, text, orientation="west"):
         super().__init__(text)
         self.orientation = orientation
 
     def paintEvent(self, event):
         painter = QStylePainter(self)
-        painter.rotate(90)
-        painter.translate(0, -1*20)
+        if self.orientation == 'west':
+            painter.rotate(90)
+            painter.translate(0, -1 * 20)
+        elif self.orientation == 'east':
+            painter.rotate(270)
+            painter.translate(-1 * self.height(), 0)
         painter.drawControl(QStyle.CE_PushButton, self.getSyleOptions())
+        self.setStyleSheet(f'''font-size: {9}pt;''')
+
 
     def sizeHint(self):
         size = super(RotatedButton, self).sizeHint()
@@ -20,8 +27,7 @@ class RotatedButton(QPushButton):
     def getSyleOptions(self):
         options = QStyleOptionButton()
         options.initFrom(self)
-        size = QtCore.QSize(80, 20)
+        size = QtCore.QSize(80, 18)
         options.rect.setSize(size)
         options.text = self.text()
         return options
-#Commit test
