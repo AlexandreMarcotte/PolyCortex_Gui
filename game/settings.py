ad99be8277ddf2c5ee4b5930ec73e3d7722e35e1
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-01-10 11:22:17 -0500

    Change pos of platforms in the game

--- game/settings.py
+++ game/settings.py
@@ -10,9 +10,10 @@ PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.5
 
 # Starting platforms
+# (x, y, w, thickness)
 PLATFORM_LIST = [
         (0, HEIGHT - 40, WIDTH, 40), (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
-        (125, HEIGHT - 350, 100, 20), (350, 200, 100, 20), (175, 100, 50, 20)]
+        (125, HEIGHT - 350, 300, 20), (350, 200, 400, 20), (175, 100, 200, 20)]
 
 # define colors
 WHITE = (255, 255, 255)
