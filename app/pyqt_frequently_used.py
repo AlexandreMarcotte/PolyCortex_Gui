9b495424e8bdcb17a978a0d5fd6627c88f335634
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-01-13 19:13:59 -0500

    Created a select file function that is used by many buttons

--- app/pyqt_frequently_used.py
+++ app/pyqt_frequently_used.py
@@ -5,6 +5,22 @@ from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QLineEdit
 
 
+def select_file(main_window, open=True, f_extension='.py'):
+    """Set open to False if you want to get the file for saving"""
+    # From: https://pythonspot.com/pyqt5-file-dialog/
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    # f_name, _ = QFileDialog.getFile
+    if open:
+        f_name, _ = QFileDialog.getOpenFileName(
+            main_window, 'QFileDialog.getOpenFileName()', '',
+            f'All Files (*);;Python Files (*{f_extension})', options=options)
+    else:
+        f_name, _ = QFileDialog.getSaveFileName(
+            main_window, 'QFileDialog.getSaveFileName()', '',
+            f'All Files (*);;Text Files (*{f_extension})', options=options)
+    return f_name
+
 def create_gr(margin=False):
     l = QGridLayout()
     if not margin:
